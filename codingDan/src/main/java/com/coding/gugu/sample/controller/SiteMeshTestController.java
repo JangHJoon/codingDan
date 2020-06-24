e7d4184ea77594439d75adb70426fe73a7fbcd3e
jangHjoon <wkdguswns103@naver.com>
2020-06-25 01:20:27 +0900

    bootstrap + sitemesh 셋팅

---
+++ codingDan/src/main/java/com/coding/gugu/sample/controller/SiteMeshTestController.java
@@ -0,0 +1,21 @@
+package com.coding.gugu.sample.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SiteMeshTestController
+{
+	private static final Logger log = LoggerFactory.getLogger(SiteMeshTestController.class);
+	
+	@RequestMapping("sitemesh")
+	public String sitemesh()
+	{
+		log.info("sitemesh test controller call....");
+		
+		return "sample_sitemesh";
+	}
+	
+}
